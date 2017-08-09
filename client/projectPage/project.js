e7f956f86b5bdce445fd3442b1a18ca8b36bde56
Matteo <matteo.margueritte@epitech.eu>
2017-08-09 13:13:55 +0200

    fixed links

--- client/projectPage/project.js
+++ client/projectPage/project.js
@@ -1,10 +1,3 @@
 /**
  * Created by blinux on 09/07/17.
  */
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-});
--- client/sideBarLeft/sideBarLeft.js
+++ client/sideBarLeft/sideBarLeft.js
@@ -1,3 +1,18 @@
 /**
  * Created by blinux on 09/07/17.
  */
+
+var $root = $('html, body');
+
+$(document).on('click', 'a', function(event)
+{
+    var $this = $(this);
+    if($this.hasClass("buttonSideNav"))
+    {
+        event.preventDefault();
+        $root.animate(
+            {
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 500);
+    }
+});
